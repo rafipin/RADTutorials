522093b8947bbff6fe00e622f4b28fac035122fd
Rafi <rafi.pincus@gmail.com>
2020-05-05 19:50:49 +1000

    Tute 6 Production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+def hello
+render html: "hello, world!"
+def show
+    @user = User.find(params[:id])
+    end
+end
 end
