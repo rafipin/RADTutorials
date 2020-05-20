e6bb3874bc207c788267e0503189003add22366a
Rafi <rafi.pincus@gmail.com>
2020-05-20 13:41:45 +1000

    added files

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
     before_save { self.email = email.downcase }
     validates :password, presence: true, length: { minimum: 6 }
     has_secure_password
+    validates:password,presence:true,length: {minimum:6},allow_nil:true
 
 def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
