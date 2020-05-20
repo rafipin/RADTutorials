4e010e86c7ca69455a85c15c48cd5c765bf7011a
Rafi <rafi.pincus@gmail.com>
2020-05-20 14:48:22 +1000

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,12 +42,15 @@ private
                                     :password_confirmation)
   end
 
-  def logged_in_user unless logged_in?
-    flash[:danger] = "Please log in."
-    redirect_to login_url
+  def logged_in_user 
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
-    
+  end
+
     def correct_user
-      @user = User.find(params[:id]) redirect_to(root_url) unless @user == current_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user(@user)
       end
 end
