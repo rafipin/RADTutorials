49d17d53a2f81522281e2b602bc9192966fc94dd
Rafi <rafi.pincus@gmail.com>
2020-05-27 16:15:52 +1000

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do   
-  get 'sessions/new'
-  get 'users/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -9,7 +7,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  post  '/signup', to: 'users#create'
   resources :users
   resources :account_activations, only: [:edit]
 end
